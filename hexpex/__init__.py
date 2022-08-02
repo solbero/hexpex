b3ee4152e632a7eb1a5379866e89dfafe86df0d6
solbero <njord.solberg@gmail.com>
2022-08-02 16:00:37 +0200

    Declare public API for package

--- hexpex/__init__.py
+++ hexpex/__init__.py
@@ -1,1 +1,12 @@
 __version__ = "0.1.0"
+
+from hexpex.hex import Axial as Axial
+from hexpex.hex import AxialFlatAdjacentDirection as AxialFlatAdjacentDirection
+from hexpex.hex import AxialFlatDiagonalDirection as AxialFlatDiagonalDirection
+from hexpex.hex import AxialPointyAdjacentDirection as AxialPointyAdjacentDirection
+from hexpex.hex import AxialPointyDiagonalDirection as AxialPointyDiagonalDirection
+from hexpex.hex import Cube as Cube
+from hexpex.hex import CubeFlatAdjacentDirection as CubeFlatAdjacentDirection
+from hexpex.hex import CubeFlatDiagonalDirection as CubeFlatDiagonalDirection
+from hexpex.hex import CubePointyAdjacentDirection as CubePointyAdjacentDirection
+from hexpex.hex import CubePointyDiagonalDirection as CubePointyDiagonalDirection
